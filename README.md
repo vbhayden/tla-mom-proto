e57eae990e0fecf3b522760f0ade0fd916625cc0
adl-trey <trey.hayden.ctr@adlnet.gov>
2019-10-21 14:08:32 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+## Prototype TLA MOM Definitions
+Helper library for defining common xAPI verbs and objects.
+
+A few things to note:
+- **This library is not directly related to or supported by the ADL Initiative IN ANY WAY.**
+- **These definitions SHOULD NOT be used outside of purely academic or research interest.**
+- **These definitions SHOULD NOT be considered final or representative of any final recommendations or specifications to be published by the ADL Initiative.**
+- **This library provides ZERO xAPI FUNCTIONALITY.  These are strictly definitions and will not assist in communication with an LRS.**
+
+### Installation
+You can install this through NPM:
+```
+npm install tla-mom-proto --save
+```
---
+++ index.js
@@ -0,0 +1,42 @@
+module.exports = {
+
+    /**
+     * Collection of all verbs compliant with the TLA MOM profile.
+     */
+    verbs: require("./lib/verbs"),
+
+    /**
+     * Object defining what Keycloak instance our adapter should target and how.
+     * 
+     * @typedef {Object} Verb 
+     * @property {string} id - IRI for this verb.
+     * @property {Object} display - Mapping between ISO language codes and the verb's representation in that language.
+     */
+    /**
+     * Checks whether or not a given verb is considered Authoritative.
+     * @param {Verb} verb - Verb to check. 
+     * @returns {boolean} True if verb.id matches a known authoritative verb.
+     */
+    isVerbAuthoritative(verb) {
+        switch (verb.id) {
+            case this.verbs.inferred.id:
+                return true;
+            case this.verbs.certified.id:
+                return true;
+            case this.verbs.verified.id:
+                return true;
+
+            default:
+                return false;
+        }
+    },
+
+    /**
+     * Checks if the statement uses a verb that is considered Authoritative.
+     * @param {Object} statement - Statement to check.
+     * @returns {boolean} True if statement.verb.id matches a known Authoritative verb.
+     */
+    isStatementAuthoritative(statement) {
+        return this.isVerbAuthoritative(statement.verb)
+    },
+}
