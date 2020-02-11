043ab8ff8236966a0d25ef27272ee5838cabdf08
adl-trey <trey.hayden.ctr@adlnet.gov>
2020-02-11 16:56:57 -0500

    added extensions and activity types

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 module.exports = {
 
     /**
-     * Collection of all verbs compliant with the TLA MOM profile.
-     */
-    verbs: require("./lib/verbs"),
-
-    /**
      * Object defining what Keycloak instance our adapter should target and how.
      * 
      * @typedef {Object} Verb 
      * @property {string} id - IRI for this verb.
      * @property {Object} display - Mapping between ISO language codes and the verb's representation in that language.
      */
+    /**
+     * Collection of all verbs compliant with the TLA MOM profile.
+     */
+    verbs: require("./lib/verbs"),
+    
+    /**
+     * Definition of activity types expected in the object.definition.type property.
+     */
+    activityTypes: require("./lib/activityTypes"),
+
+    /**
+     * Collection of all verbs compliant with the TLA MOM profile.
+     */
+    resultExtensions: require("./lib/resultExtensions"),
+
+    /**
+     * Collection of all verbs compliant with the TLA MOM profile.
+     */
+    contextExtensions: require("./lib/contextExtensions"),
+    
     /**
      * Checks whether or not a given verb is considered Authoritative.
      * @param {Verb} verb - Verb to check. 
